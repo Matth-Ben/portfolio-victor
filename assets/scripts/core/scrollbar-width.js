a963e3a362056d90e24f169c9f4332b6ad02ede7
Martin Paul <martin@danka.fr>
2023-10-19 10:05:23 +0200

    save

--- assets/scripts/core/scrollbar-width.js
+++ assets/scripts/core/scrollbar-width.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 
 export default function()
 {
     document.documentElement.style.setProperty( '--scrollbar-width' , (window.innerWidth - document.documentElement.clientWidth) + "px")
 }
+=======
+export default function()
+{
+    document.documentElement.style.setProperty( '--scrollbar-width' , (window.innerWidth - document.documentElement.clientWidth) + "px")
+}
+>>>>>>> 3c1cccc (save)
