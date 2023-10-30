7a8b31582b951226ced3fa73d5c44ad3f6cf16e4
Martin Paul <martin@danka.fr>
2023-10-30 15:07:18 +0100

    🐛 [COOKIES] remove notice

--- custom/cookies/index.php
+++ custom/cookies/index.php
@@ -3,26 +3,28 @@
 include get_template_directory() . '/custom/cookies/_register-options-page.php';
 include get_template_directory() . '/custom/cookies/_register-settings.php';
 
-$path = get_template_directory_uri() . '/custom/cookies';
-
-wp_enqueue_script( 'app_cookies_init', $path . '/assets/scripts/index.js' );
-wp_localize_script( 'app_cookies_init', 'cookies_data', array(
-    'text' => array(
-        'title' => get_option( 'danka__cookies__title' ),
-        'description' => get_option( 'danka__cookies__description' ),
-        'link' => get_option( 'danka__cookies__link' ),
-    ),
-    'youtube' => array(
-        'name' => 'Youtube',
-        'script' => null,
-        'is_active' => get_option( 'danka__cookies__youtube' ) ? true : false
-    ),
-    'google_analytics' => array(
-        'name' => 'Google Analytics',
-        'script' => get_option( 'danka__cookies__google_analytics__script' ),
-        'is_active' => get_option( 'danka__cookies__google_analytics' ) ? true : false
-    ),
-) );
+add_action( 'wp_enqueue_scripts', function()
+{
+    $path = get_template_directory_uri() . '/custom/cookies';
+    wp_enqueue_script( 'app_cookies_init', $path . '/assets/scripts/index.js' );
+    wp_localize_script( 'app_cookies_init', 'cookies_data', array(
+        'text' => array(
+            'title' => get_option( 'danka__cookies__title' ),
+            'description' => get_option( 'danka__cookies__description' ),
+            'link' => get_option( 'danka__cookies__link' ),
+        ),
+        'youtube' => array(
+            'name' => 'Youtube',
+            'script' => null,
+            'is_active' => get_option( 'danka__cookies__youtube' ) ? true : false
+        ),
+        'google_analytics' => array(
+            'name' => 'Google Analytics',
+            'script' => get_option( 'danka__cookies__google_analytics__script' ),
+            'is_active' => get_option( 'danka__cookies__google_analytics' ) ? true : false
+        ),
+    ) );
+} );
 
 add_filter( 'script_loader_tag', function( $tag, $handle, $source ) {
 
