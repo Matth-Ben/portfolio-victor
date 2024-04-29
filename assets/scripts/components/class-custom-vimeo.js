573d7882e30de783413aa86ca76a2cf3a6e67b4a
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-04-29 23:22:53 +0200

    :lipstick: [CUSTOM-VIDEO] Fix js custom progress bar + add button sound

--- assets/scripts/components/class-custom-vimeo.js
+++ assets/scripts/components/class-custom-vimeo.js
@@ -14,6 +14,9 @@ class CustomVimeo
         };
 
         const player = new Vimeo(element, options);
+        var soundButton = document.querySelector('#sound-button');
+        updateProgressBar(0);
+
 
         player.on('timeupdate', function(data) {
             const currentTime = data.seconds;
@@ -28,30 +31,44 @@ class CustomVimeo
 
         function updateProgressBar(progress) {
             // Update the progress bar element with the new progress value
-            const progressBar = document.querySelector('.custom-progress-bar-content');
+            const progressBar = document.querySelector('.custom-progress-bar--content');
             progressBar.style.width = progress + '%';
         }
 
         function setVideoProgress(progress) {
-            const duration = player.getDuration();
-            const currentTime = (progress / 100) * duration;
-            player.setCurrentTime(currentTime);
+            player.getDuration().then(duration => {
+                const currentTime = (progress / 100) * duration;
+                player.setCurrentTime(currentTime);
+            });
         }
 
         const progressBar = document.querySelector('.custom-progress-bar');
-        const progressBarContent = document.querySelector('.custom-progress-bar-content');
         progressBar.addEventListener('click', function(event) {
-            const progressBarWidth = progressBarContent.offsetWidth;
+            const progressBarWidth = progressBar.offsetWidth;
             const clickX = event.offsetX;
-            console.log(clickX);
             const progress = (clickX / progressBarWidth) * 100;
             setVideoProgress(progress);
+        });
+
+        // Gérez les clics sur le bouton de son
+        soundButton.addEventListener('click', function() {
+            // Obtenez l'état actuel du son
+            player.getVolume().then(function(volume) {
+                if (volume > 0) {
+                    // Si le son est activé, désactivez-le
+                    player.setVolume(0);
+                    soundButton.textContent = 'Sound OFF';
+                } else {
+                    // Si le son est désactivé, activez-le
+                    player.setVolume(1);
+                    soundButton.textContent = 'Sound ON';
+                }
+            });
         });
     }
 
     init() {
         player.play();
-        this.updateProgressBar(0);
     }
 }
 
